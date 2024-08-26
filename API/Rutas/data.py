231c40661b47fb78e182e26bcab46692c380cbd2
LorenorMc <letloc3621@gmail.com>
2024-08-26 14:56:40 +0700

    data.py

---
+++ API/Rutas/data.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['POST'])
+def save_data():
+    data = request.get_json()
+    # Xử lý dữ liệu và lưu trữ vào cơ sở dữ liệu
+    #...
+    return jsonify({'message': 'Data has been saved successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
