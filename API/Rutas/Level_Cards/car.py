c12ef753edbc4e67493de236010fbc24642f0c91
LorenorMc <letloc3621@gmail.com>
2024-08-26 09:57:31 +0700

    car.py

--- API/Rutas/Level_Cards/car.py
+++ API/Rutas/Level_Cards/car.py
@@ -22,9 +22,9 @@ def level(avatar: str, username: str, level:int):
    
     poppins = Font.poppins(size=30)
     desplazamiento_x = 17  
-    editor.rectangle((110, 16), width=430, height=100, fill="#25262a", radius=15)
+    editor.rectangle((110, 16), width=250, height=100, fill="#25262a", radius=15)
     editor.ellipse((30 + desplazamiento_x, 11), width=110, height=110, outline="white", stroke_width=8)
-    editor.text((180, 57), f"{username} Đã lên level {level}", font=poppins, color="white")
+    editor.text((180, 57), f"Level Up {level}! 🎉", font=poppins, color="white")
     editor.paste(profile.image, (35 + desplazamiento_x, 16))
 
     
