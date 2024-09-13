7e55da6d082aecf510e5d65e1c4d1089c49a734e
LorenorMc <letloc3621@gmail.com>
2024-09-13 17:27:30 +0700

    Update level_card.py

--- API/Endpoint/Level/level_card.py
+++ API/Endpoint/Level/level_card.py
@@ -2,10 +2,10 @@ from easy_pil import Editor, Canvas, Font
 from io import BytesIO
 from fastapi import APIRouter, Response, HTTPException
 from API.Funciones_API.convert_k_m import abreviar_numero
-
+from flask import Flask, request, jsonify
 
 import requests
-
+    
 router = APIRouter()
 
 @router.get("/api/level/card/")
@@ -48,6 +48,7 @@ def rank(avatar: str, username: str, level: str, req: str, xp: str, color_bg: st
     background.image.save(img_buffer, format="PNG")
     img_buffer.seek(0)
 
-    return Response(content=img_buffer.getvalue(), media_type="image/png")
-    
+    ORIGINAL_API_URL = Response(content=img_buffer.getvalue(), media_type="image/png")
+    response = requests.get(ORIGINAL_API_URL)
 
+    return jsonify(response.json()), response.status_code
