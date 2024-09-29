ee34dc0fa85337c9ef0216c7cf5e92c0fe0d971a
Marlernix Mc <letloc3621@gmail.com>
2024-09-29 09:03:23 +0700

    Update level_card.py

--- API/Endpoint/Level/level_card.py
+++ API/Endpoint/Level/level_card.py
@@ -39,7 +39,7 @@ def rank(avatar: str, username: str, level: str, req: str, xp: str, color_bg: st
     background.rectangle((150, 80 + 4), width=145, height=2, fill=color_bg)
 
     poppins = Font.poppins(size=35)
-    background.text((150, 37 + 4), f"{username}", font=poppins, color=f"{color_font}")
+    background.text((150, 37 + 4), username, font=poppins, color=f"{color_font}")
 
     poppins = Font.poppins(size=25)
     background.text((145, 107), f"Level: {int(level)}          XP: {abreviar_numero(int(xp))} / {abreviar_numero(int(req))}", font=poppins, color=f"{color_font}")
@@ -49,3 +49,5 @@ def rank(avatar: str, username: str, level: str, req: str, xp: str, color_bg: st
     img_buffer.seek(0)
 
     return Response(content=img_buffer.getvalue(), media_type="image/png")
+    
+
