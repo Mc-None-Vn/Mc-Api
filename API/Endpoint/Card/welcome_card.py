455c9b018acada1e78b92c5729391b3b1ed81cbe
Marlernix Mc <letloc3621@gmail.com>
2024-10-24 18:21:37 +0700

    Update welcome_card.py

--- API/Endpoint/Card/welcome_card.py
+++ API/Endpoint/Card/welcome_card.py
@@ -13,7 +13,7 @@ def get_custom_image(avatar: str, background: str, ctx1: str="WELCOME", ctx2: st
         avatar_response = requests.get(avatar)
         if avatar_response.status_code != 200:
             raise HTTPException(status_code=400, detail="Failed to download avatar image.")
-        avatar_image = Editor(BytesIO(avatar_response.content)).resize((175, 175)).circle_image()
+        avatar_image = Editor(BytesIO(avatar_response.content)).resize((160, 160)).circle_image()
 
         
         background_response = requests.get(background)
@@ -23,7 +23,7 @@ def get_custom_image(avatar: str, background: str, ctx1: str="WELCOME", ctx2: st
 
         
         poppins = Font.poppins(size=50, variant="bold")
-        poppins_small = Font.poppins(size=35, variant="regular")
+        poppins_small = Font.poppins(size=37.5, variant="regular")
 
        
         horizontal_shift = 63
@@ -32,8 +32,8 @@ def get_custom_image(avatar: str, background: str, ctx1: str="WELCOME", ctx2: st
         editor = Editor(background_image)
 
         
-        editor.paste(avatar_image.image, (250 + horizontal_shift, 90))
-        editor.ellipse((250 + horizontal_shift, 90), 150, 150, outline="white", stroke_width=5)
+        editor.paste(avatar_image.image, (320 + horizontal_shift, 80))
+        editor.ellipse((320 + horizontal_shift, 80), 160, 160, outline="white", stroke_width=5)
 
        
         editor.text((320 + horizontal_shift, 260), ctx1, color="white", font=poppins, align="center")
