b397d3ee80675818998713c8ef933898adaa2b7b
Marlernix Mc <letloc3621@gmail.com>
2024-10-26 09:46:10 +0700

    Update welcome_card.py

--- API/Endpoint/Card/welcome_card.py
+++ API/Endpoint/Card/welcome_card.py
@@ -7,18 +7,18 @@ import logging
 router = APIRouter()
 
 @router.get("/api/card/welcome/")
-def get_custom_image(avatar: str, background: str, ctx1: str="WELCOME", ctx2: str="xquab#0"):
+def get_custom_image(avatar: str, background: str, title: str="Welcome", description: str="Now has 10 members"):
     try:
         
         avatar_response = requests.get(avatar)
         if avatar_response.status_code != 200:
-            raise HTTPException(status_code=400, detail="Failed to download avatar image.")
+            raise HTTPException(status_code=400, detail="Khong tai đuoc anh dai dien.")
         avatar_image = Editor(BytesIO(avatar_response.content)).resize((180, 180)).circle_image()
 
         
         background_response = requests.get(background)
         if background_response.status_code != 200:
-            raise HTTPException(status_code=400, detail=f"Failed to download background image. Status code: {background_response.status_code}, Reason: {background_response.reason}")
+            raise HTTPException(status_code=400, detail=f"Khong tai duoc hinh nen. Ma trang thai: {background_response.status_code}, Ly do: {background_response.reason}")
         background_image = Editor(BytesIO(background_response.content)).resize((800, 400)).image
 
         
@@ -36,8 +36,8 @@ def get_custom_image(avatar: str, background: str, ctx1: str="WELCOME", ctx2: st
         editor.ellipse((230 + horizontal_shift, 65), 180, 180, outline="white", stroke_width=5)
 
        
-        editor.text((320 + horizontal_shift, 275), ctx1, color="white", font=poppins, align="center")
-        editor.text((320 + horizontal_shift, 332.5), ctx2, color="white", font=poppins_small, align="center")
+        editor.text((320 + horizontal_shift, 275), title, color="white", font=poppins, align="center")
+        editor.text((320 + horizontal_shift, 332.5), description, color="white", font=poppins_small, align="center")
 
         
         img_buffer = BytesIO()
@@ -48,5 +48,5 @@ def get_custom_image(avatar: str, background: str, ctx1: str="WELCOME", ctx2: st
         return Response(content=img_buffer.getvalue(), media_type="image/png")
     
     except Exception as e:
-        logging.error(f"Error generating image: {e}")
+        logging.error(f"Loi khi tao hinh anh: {e}")
         raise HTTPException(status_code=500, detail=str(e))
